5ae4db0e088ab3d361f82e318558347a0f875d39
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-01 02:21:47 -0700

    routes minus update complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,20 +37,21 @@ router.post('/', async (req, res) => {
   // create a new category
   try{
     const categoryData = await Category.create(req.body)
+    res.status(200).json(categoryData)
   } catch(err){
     res.status(500).json(err)
   }
 });
 
-// router.put('/:id', async (req, res) => {
-//   // update a category by its `id` value
-//   try{
-//     const categoryData = await Category.findByPk(req.params.id,)
-//     res.status(200).json(categoryData)
-//   } catch(err){
-//     res.status(500).json(err)
-//   }
-// });
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try{
+    const categoryData = await Category.findByPk(req.params.id,)
+    res.status(200).json(categoryData)
+  } catch(err){
+    res.status(500).json(err)
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
